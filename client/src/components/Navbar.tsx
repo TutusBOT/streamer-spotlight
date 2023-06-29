ffb7c3a5905f924a107e6e19d5d7e902563c2ccb
TutusBOT <barte77pl@gmail.com>
2023-06-29 23:16:34 +0200

    feat: add Navbar component

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+function Navbar() {
+	return (
+		<nav className="absolute top-2 left-2 flex items-center justify-start text-2xl bg-gray-800 text-white border-[1px] border-white py-2 px-4 rounded-xl  hover:text-purple-700 hover:border-purple-700 transition-colors">
+			<a href="/">Home</a>
+		</nav>
+	);
+}
+export default Navbar;
