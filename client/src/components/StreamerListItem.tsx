18aac196d9abfe9126784f20117873da959ad1da
TutusBOT <barte77pl@gmail.com>
2023-07-02 15:54:09 +0200

    chore: improve rwd

--- client/src/components/StreamerListItem.tsx
+++ client/src/components/StreamerListItem.tsx
@@ -32,7 +32,7 @@ function StreamerListItem({
 	const isUpvoted = currentVote?.vote === "upvote";
 	const isDownvoted = currentVote?.vote === "downvote";
 	return (
-		<li className="grid w-full grid-cols-3 gap-4 px-4 py-4 text-xl transition-colors sm:px-8 hover:bg-gray-700">
+		<li className="grid w-full grid-cols-3 gap-4 px-2 py-4 text-lg transition-colors sm:text-xl sm:px-4 lg:px-8 hover:bg-gray-700">
 			<Link
 				to={`/streamer?id=${streamer._id}`}
 				className="relative w-fit before:opacity-0 before:transition-opacity hover:before:opacity-100 before:w-full before:bg-white before:absolute before:bottom-0 before:left-0 before:h-[1px]"
@@ -42,7 +42,7 @@ function StreamerListItem({
 			<div>{streamer.platform}</div>
 			<div className="flex items-center gap-2 place-self-end">
 				<button
-					className="transition-colors hover:bg-purple-600"
+					className="p-1 transition-colors rounded-lg hover:bg-purple-600"
 					onClick={() => {
 						handleVote("upvote", streamer._id, currentVote);
 					}}
@@ -53,9 +53,9 @@ function StreamerListItem({
 						alt="arrow"
 					/>
 				</button>
-				{streamer.votes}
+				<div className="min-w-[2ch] text-center">{streamer.votes}</div>
 				<button
-					className="transition-colors hover:bg-orange-400"
+					className="p-1 transition-colors rounded-lg hover:bg-orange-400"
 					onClick={() => {
 						handleVote("downvote", streamer._id, currentVote);
 					}}
