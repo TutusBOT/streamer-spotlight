2d6fe416239bf9564e165169619187fb86028da1
TutusBOT <barte77pl@gmail.com>
2023-07-02 15:44:06 +0200

    fix: visuals

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 function Error({ message }: { message: string }) {
 	return (
-		<div className="w-screen h-screen text-2xl text-white bg-gray-900">
+		<div className="absolute top-0 left-0 z-50 w-screen h-screen text-2xl text-white bg-gray-900">
 			<Navbar />
 			<p className="absolute text-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-gray-950 border-[1px] border-red-700 rounded-xl p-4">
 				Something went wrong. <br />
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
-
 function Navbar() {
 	return (
 		<nav className="absolute top-2 left-2 flex items-center justify-start text-2xl bg-gray-800 text-white border-[1px] border-white  rounded-xl  hover:text-purple-700 hover:border-purple-700 transition-colors">
-			<Link to="/" className="px-4 py-2">
+			<a href="/" className="px-4 py-2">
 				Home
-			</Link>
+			</a>
 		</nav>
 	);
 }
